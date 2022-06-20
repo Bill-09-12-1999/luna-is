72a3d04954b252df5075abfd54d3e250cf3d340f
Bill-09-12-1999 <104749085+Bill-09-12-1999@users.noreply.github.com>
2022-06-21 04:07:39 +0800

    Update connection.php

--- login/pages/report/connection.php
+++ login/pages/report/connection.php
@@ -6,8 +6,8 @@
 //  $con = mysqli_connect('localhost','id18870513_burgos','6WdElkqIGnEK-aqF','id18870513_luna') or die(mysqli_error());
 
 
-$conn=mysqli_connect('localhost','id18870513_burgos','6WdElkqIGnEK-aqF','id18870513_luna') or die("Could not connect");
-mysqli_select_db($conn,'id18870513_luna') or die("could not connect database");
+$conn=mysqli_connect('remotemysql.com','7LwsksBaaS','Kq4pKA9wKF','7LwsksBaaS') or die("Could not connect");
+mysqli_select_db($conn,'7LwsksBaaS') or die("could not connect database");
 
 
 			date_default_timezone_set("Asia/Manila");
